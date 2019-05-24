e97e573b1a83f30df95bb2a6513abf9970edfd48
Jason Sanchez <jason@xao.live>
2019-05-24 13:33:30 -0600

    TaskListController delegate/dataSource

--- RxPriority/RxPriority/Controllers/TaskListController.swift
+++ RxPriority/RxPriority/Controllers/TaskListController.swift
@@ -9,22 +9,30 @@
 import UIKit
 
 class TaskListController: UIViewController {
-
+    
+    @IBOutlet weak var prioritySegment: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension TaskListController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GoodCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    
 }
