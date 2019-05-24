d873d96eb29b192964ef76f6b8f7433cf036883b
Jason Sanchez <jason@xao.live>
2019-05-24 13:13:59 -0600

    Storyboard

---
+++ RxPriority/RxPriority/Controllers/TaskListController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskListController.swift
+//  RxPriority
+//
+//  Created by Jason Sanchez on 5/24/19.
+//  Copyright © 2019 Jason Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class TaskListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
