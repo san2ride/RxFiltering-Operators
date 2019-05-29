f983a5f3dbb0ddbef140e036dcaa58a803199c3f
Jason Sanchez <jason@xao.live>
2019-05-29 17:46:28 -0600

    AddTaskController UI configured

---
+++ RxPriority/RxPriority/Controllers/AddTaskController.swift
@@ -0,0 +1,20 @@
+//
+//  AddTaskController.swift
+//  RxPriority
+//
+//  Created by Jason Sanchez on 5/24/19.
+//  Copyright © 2019 Jason Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+}
